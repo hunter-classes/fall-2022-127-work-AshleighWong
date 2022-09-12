73995a5fe9dfd255f42c673f023ceb4916e251b4
Ashleigh W <47904190+AshleighWong@users.noreply.github.com>
2022-09-12 13:29:48 +0000

    created strings_review.py

---
+++ code/strings/strings_review.py
@@ -0,0 +1,19 @@
+s1 = "Hello World"
+s2 = 'another \nstring' #n is a way of representing its character which is the new line 
+
+s3 = """
+This is a multiline string
+we use triple quotes for that
+"""
+#'Joh)n\'s book'
+
+s4 = s1+s2# string catenation
+print(s4)
+print(s1*3) # prints string 3 times
+
+print(len(s1))
+print(len("abcde"))
+
+print(s1.upper())
+
+s1.find("H") #gives us the location of H
