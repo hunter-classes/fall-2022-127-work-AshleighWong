fabc9d6ee60a59cbcd78c00283372660cf5990d8
Ashleigh W <47904190+AshleighWong@users.noreply.github.com>
2022-09-19 01:06:34 +0000

    created piglatin and piglatin.py

--- additionalcode/examples.py
+++ additionalcode/examples.py
@@ -14,7 +14,6 @@ def is_odd(n):
 # will give the opposite of not even. its odd if it it not even 
 
 def isRightAngle(a,b,c): #c is the longest
-  sum = a*a + b*b
   return a*a + b*b == c*c
   
 def isRightAngle2(a,b,c):
@@ -22,11 +21,6 @@ def isRightAngle2(a,b,c):
           isRightAngle(b,c,a) or \
           isRightAngle(a,c,b)
   
-
-  
-
-
-
 
 print('even tests')
 result = is_even(10)
@@ -41,3 +35,26 @@ result = is_odd(11)
 print('result for 11 is: ', result)
 
 print(isRightAngle)
+
+#STRINGS PRACTICE
+#input a string in the form "first last"
+#return a string in the form F.last"
+
+def initialize(name):
+  result = first = name[0]
+#isolate, uppercase and add first init to result
+  first = first.upper()
+  result = result + first +"."
+#find the last name (after space), cap it and add to the result
+  location = name.find(" ")
+  last = name[location+1:].capitalize()
+  result = result + " " + last
+  return result
+
+  print(result) 
+
+
+#input string in the form "first last"
+#return "last, first last"
+def bondify(name2):
+  return 
---
+++ piglatin/piglatin.py
@@ -0,0 +1,67 @@
+def initialize(name):
+  result = first = name[0]
+#isolate, uppercase and add first init to result
+  first = first.upper()
+  result = result + first +"."
+#find the last name (after space), cap it and add to the result
+  location = name.find(" ")
+  last = name[location+1:].capitalize()
+  result = result + " " + last
+  return result
+
+print(initialize(input("type first and last name")))
+
+# Test initialize
+result = initialize("james bond")
+print("james bond --> ",result)
+
+"""
+input: a string in the form "first last"
+returns: a string in the form "Last, First Last"
+"""
+#bondify
+def bondify(name):
+  result = first = name[0]
+  first = first.capitalize()
+
+  location =name.find("")
+  last = name[location+1:].capitalize()
+  result = last + "," + first + " " + last
+  return result
+
+print(bondify(input("type first and last name")))
+  
+
+
+
+"""
+input: a string representing a word
+returns: a new string with the word converted to piglatin
+
+Rules:
+if the first letter is a consonent, move it from the start to the end and add "ay"
+so "hello" becomes "ellohay"
+
+If the first letter is a vowel, just add "yay" to the end
+
+try to also handle upper case words
+
+"""
+#check if first letter is a vowel 
+
+def vowel_checker(letter):
+  vowel = ('a','e','i','o','u','y')
+  if letter in vowel:
+    return True
+  else:
+    return False
+
+#piglatin function 
+def piglatin(word):
+  if vowel_checker(word[0]) == True:
+    print(word + "yay")
+  else:
+    #need to find length of word
+    print(word[1:(len(word))] + word[:1] + "ay")
+
+piglatin(input("type in a word you want in piglatin! "))
