b5e4aa9c47ec59d5431f2d9650bd58dd04c0b796
Ashleigh W <47904190+AshleighWong@users.noreply.github.com>
2022-10-13 08:54:41 +0000

    created listlab.py and did exercises

---
+++ chapter-10/listlab.py
@@ -0,0 +1,64 @@
+#Write a function to find the smallest value in a listKfind smallest in a list of items
+
+def listKfind(n):
+  list1 = []
+  number = int(input("Enter number of elements in list"))
+  for i in range(1, number + 1):
+  elements = int(input("Enter elements:"))
+  
+
+#Write a function that returns a new list that contains
+#all the odd items in the original list
+  
+def odd(my_list):
+  list = []
+  for i in list:
+    if i%2 != 0:
+      list.append(i)
+
+ return(list)
+  
+#Write a function that takes a string and returns a new string where all the words are capitalized
+
+def capital(string_word):
+  return[string_word.capitalize]
+  
+#Write a function that takes a string and returns a new string with every word that's longer than 5 character turned into upper case
+
+#Write a function that takes a list of numbers and returns it back squared
+  
+def square(list):
+    return [i ** 2 for i in list]
+
+#Write a function that takes two lists of numbers and returns a new list where each item is the corresponding values of the originallists added together. Ex [1,2,3] and [10,20,30] would return the list [11,22,33]
+
+
+
+#10.10
+
+def countwords(1st):
+  count = 0
+  for i in 1st:
+   if len(i) == 4
+    count = count + 1
+ return count 
+    
+#10.11
+
+def sumUntilEven(1st):
+    sum = 0
+    ind = 0 #index
+    while ind < len(lst) and lst[ind] % 2 != 0:
+        sum = sum + lst[ind]
+        ind = ind + 1
+    return sum
+
+#10.12
+def counts(lst):
+  count = 0
+  for i in 1st:
+   if i = "sam":
+    count = count + 1
+  return 1st
+  
+  
