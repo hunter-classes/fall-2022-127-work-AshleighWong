bde98c00cddfbda440d5c07003f00774dd5f13e0
Ashleigh W <47904190+AshleighWong@users.noreply.github.com>
2022-11-02 03:37:17 +0000

    madlibs.py edit milestone

--- project-01-madlibs/madlib.py
+++ project-01-madlibs/madlib.py
@@ -8,14 +8,14 @@ VERBS = ['stab', 'fear', 'cook', 'swat', 'fight', 'harass', 'explode', 'hit']
 
 NOUNS = ['toes', 'idiot', 'dog','cat','bathroom','butt','cockroach','poo']
 
-HEROS = ['Kenzie', 'Chelsea', 'Jasmine']
+HEROS = ['Kenzie','Chelsea', 'Jasmine']
 
 f = open("madlibs.txt", "r")
 content = f.read()
 content_words = content.split()
 
 #create function to identify and replace 
-def change_adjective():
+def adjective_changer():
     
   
   
--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,11 +1,11 @@
 import random 
 
 #open, read, and write file
-f = open("madlibs.txt", "r") # madlibs.txt = locaton of the file. Opens file and stores in f. "r" opens in read mode. "w" opens in write mode. "a" appends new content at end of file.
-content = f.read()
+f = open("madlibs.txt", "r") # madlibs.txt = locaton of the file. Opens file and stores in f. "r" opens in read mode.
+content = f.read() 
 content_words = content.split() #seperates charcters with white spaces between them
 
-adj_list = ['stinky' , 'ugly', 'wild', 'moldy', 'violent', 'angry', 'sick', 'filthy']
+adj_list = ['stinky' , 'ugly', 'wild', 'moldy', 'violent', 'angry', 'sick', 'filthy'] #adjective list
 
 verb_list = ['stab', 'fear', 'cook', 'swat', 'fight', 'harass', 'explode', 'hit']
 
@@ -13,11 +13,10 @@ noun_list = ['toes', 'idiot', 'dog','cat','bathroom','butt','cockroach','poo']
 
 heros_list = ['Kenzie', 'Chelsea', 'Jasmine']
 
-def change_adjective(adjective):
-    for word in content_words:
-      if content_words == '<ADJECTIVE>':
-        result = content_words.replace(random.choice(adj_list))
-    return result
+def change_adjective():
+    for i in range(len(content_words)): #getting length of the content words(the text file) and setting it to the range of the for loop.
+      if content_words[i] == '<ADJECTIVE>':# content_words[i] means the element at index 'i' of list. 'i' is the variable created for the loop
+        content_words[i] = random.choice(adj_list) #replaces the index position when the statement above is True with random element from adj_list. 
+    return content_words
 
-    print(result)
-
+print(change_adjective())
